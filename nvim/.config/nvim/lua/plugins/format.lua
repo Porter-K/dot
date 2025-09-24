cd740ca656834e5244dd9f98e589ca0ac2bca8e4
Kyle Porter <kyle.porter04@pm.me>
2025-09-24 16:06:07 -0700

    nvim: switch to conform for formatting

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,64 +1,30 @@
 return {
     {
-        "mhartington/formatter.nvim",
+        "stevearc/conform.nvim",
+        opts = {},
         keys = {
-            { "<Leader>cf", ":Format<CR>" },
+            {
+                "<leader>cf",
+                function()
+                    require("conform").format()
+                end,
+                desc = "Format",
+            },
         },
         config = function()
-            -- Utilities for creating configurations
-            local util = require("formatter.util")
-
-            -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-            require("formatter").setup({
-                -- Enable or disable logging
-                logging = true,
-                -- Set the log level
-                log_level = vim.log.levels.WARN,
-                -- All formatter configurations are opt-in
-                filetype = {
-                    -- Formatter configurations for filetype "lua" go here
-                    -- and will be executed in order
-                    lua = {
-                        -- "formatter.filetypes.lua" defines default configurations for the
-                        -- "lua" filetype
-                        require("formatter.filetypes.lua").stylua,
-
-                        -- You can also define your own configuration
-                        function()
-                            -- Supports conditional formatting
-                            if
-                                util.get_current_buffer_file_name()
-                                == "special.lua"
-                            then
-                                return nil
-                            end
-
-                            -- Full specification of configurations is down below and in Vim help
-                            -- files
-                            return {
-                                exe = "stylua",
-                                args = {
-                                    "--search-parent-directories",
-                                    "--stdin-filepath",
-                                    util.escape_path(
-                                        util.get_current_buffer_file_path()
-                                    ),
-                                    "--",
-                                    "-",
-                                },
-                                stdin = true,
-                            }
-                        end,
-                    },
-
-                    -- Use the special "*" filetype for defining formatter configurations on
-                    -- any filetype
-                    ["*"] = {
-                        -- "formatter.filetypes.any" defines default configurations for any
-                        -- filetype
-                        require("formatter.filetypes.any").remove_trailing_whitespace,
-                        -- Remove trailing whitespace without 'sed'
-                        -- require("formatter.filetypes.any").substitute_trailing_whitespace,
+            local conform = require("conform")
+            conform.setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    -- Conform will run multiple formatters sequentially
+                    python = { "isort", "black" },
+                    -- You can customize some of the format options for the filetype (:help conform.format)
+                    rust = { "rustfmt", lsp_format = "fallback" },
+                    -- Conform will run the first available formatter
+                    javascript = {
+                        "prettierd",
+                        "prettier",
+                        stop_after_first = true,
                     },
                 },
             })
